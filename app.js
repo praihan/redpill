f0b54c55ecaab66f97150802a9ab7d1f4a5deb78
Pranjal Raihan <pranjal@pranjal-raihan.com>
2015-10-17 00:44:54 -0400

    Add better handling of ports

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var express = require('express');
 app = express();
 app.use(express.static('public'));
 
-var port = process.argv[2] ? process.argv[2] : 80;
-app.listen(port);
+var port = process.argv[2] ? process.argv[2] : 0;
+if (port === 0) {
+  port = process.env.PORT || 1337;
+}
+app.set('port', port);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
